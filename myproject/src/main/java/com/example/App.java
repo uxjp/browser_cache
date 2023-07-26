893a72412aea9bfaa2711911f8b2a6bdb49125a8
JoaoPedro <joaopedro1172@gmail.com>
2023-07-26 16:17:45 -0300

    test

--- myproject/src/main/java/com/example/App.java
+++ myproject/src/main/java/com/example/App.java
@@ -1,15 +1,31 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App {
     public static void main( String[] args ) {
         Person person = new Person();
         person.setName("John");
         person.setAge(30);
-        System.out.println( "Hello World!" );
-        System.out.println(person);
+
+        // Convert Person instance to JSON string
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(person);
+            System.out.println("Person as JSON: " + jsonString);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        // Parse JSON string back to a Person object
+        String jsonInput = "{\"name\":\"Jane\",\"age\":25}"; // Replace with your JSON input
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Person parsedPerson = objectMapper.readValue(jsonInput, Person.class);
+            System.out.println("Parsed Person: " + parsedPerson.getName() + ", " + parsedPerson.getAge());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
