3a2d6396e4b2be1628e5bfad728508c52add62f6
JoaoPedro <joaopedro1172@gmail.com>
2023-07-26 13:15:49 -0300

    Person mock class

--- myproject/src/main/java/com/example/App.java
+++ myproject/src/main/java/com/example/App.java
@@ -4,10 +4,12 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
         System.out.println( "Hello World!" );
+        System.out.println(person);
     }
 }
---
+++ myproject/src/main/java/com/example/Person.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
+    }
+}
